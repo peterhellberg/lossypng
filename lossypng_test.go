487c1dadb1bcba2ef4c482838a3c78ddb873401a
Peter Hellberg <peter@c7.se>
2015-07-15 19:44:27 +0200

    Initial version of the lossypng library
    Heavily based on the command line tool with the same name.
    
    https://github.com/foobaz/lossypng

---
+++ lossypng_test.go
@@ -0,0 +1,44 @@
+package lossypng
+
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestOptimize(t *testing.T) {
+	m, _ := openTestImage("_testdata/dakar.png")
+	o := Optimize(m, RGBAConversion, 10)
+	g := Optimize(m, GrayscaleConversion, 10)
+
+	savePNGImage(m, "original.png")
+	savePNGImage(o, "optimized.png")
+	savePNGImage(g, "grayscale.png")
+}
+
+func openTestImage(fn string) (image.Image, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	m, _, err := image.Decode(f)
+
+	return m, err
+}
+
+// Helper method to debug processed image
+func savePNGImage(m image.Image, fn string) {
+	log.Printf("saving: %s", fn)
+	if f, err := os.Create(fn); err != nil {
+		log.Printf("unable to create %s: %v", fn, err)
+	} else {
+		defer f.Close()
+
+		enc := png.Encoder{png.BestCompression}
+		enc.Encode(f, m)
+	}
+}
