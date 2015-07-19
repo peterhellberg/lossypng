8df97a9b17605789608ff7ca37c430cdc85f5461
Peter Hellberg <peter@c7.se>
2015-07-19 22:18:57 +0200

    Tests using new pngBuffer helper function

--- lossypng_test.go
+++ lossypng_test.go
@@ -1,6 +1,8 @@
 package lossypng
 
 import (
+	"bufio"
+	"bytes"
 	"image"
 	"image/png"
 	"log"
@@ -11,11 +13,18 @@ import (
 func TestOptimize(t *testing.T) {
 	m, _ := openTestImage("_testdata/dakar.png")
 	o := Optimize(m, RGBAConversion, 8)
-	g := Optimize(m, GrayscaleConversion, 20)
+	ob := pngBuffer(o)
 
-	savePNGImage(m, "/tmp/lossypng-original.png")
-	savePNGImage(o, "/tmp/lossypng-optimized-8.png")
-	savePNGImage(g, "/tmp/lossypng-grayscale-20.png")
+	if got, max := ob.Len(), 179725; got > max {
+		t.Errorf("ob.Len() = %d, expected max %d", got, max)
+	}
+
+	g := Optimize(m, GrayscaleConversion, 20)
+	gb := pngBuffer(g)
+
+	if got, max := gb.Len(), 26265; got > max {
+		t.Errorf("gb.Len() = %d, expected max %d", got, max)
+	}
 }
 
 func openTestImage(fn string) (image.Image, error) {
@@ -28,6 +37,18 @@ func openTestImage(fn string) (image.Image, error) {
 	m, _, err := image.Decode(f)
 
 	return m, err
+}
+
+func pngBuffer(m image.Image) bytes.Buffer {
+	var b bytes.Buffer
+
+	w := bufio.NewWriter(&b)
+
+	png.Encode(w, m)
+
+	w.Flush()
+
+	return b
 }
 
 // Helper method to debug processed image
